c763a22c5f0e3b38f33664c9ce4d317a4afd6dcc
guglothkumari <guglothkumari@gmail.com>
2026-09-21 10:54:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def register():
     return render_template('successful.html', name=name,  year=year)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
